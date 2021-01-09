45bd02e813654bbf7ce1ec91a98cd99ac4a30cf1
Lin Chen <40065651+chenlindolian@users.noreply.github.com>
2021-01-08 19:21:32 -0500

    Weighted averaging weight for optimal SNR

---
+++ AverageEchoWeight.m
@@ -0,0 +1,13 @@
+function weight = AverageEchoWeight(GREMag,TE)
+ 
+Magtemp = zeros(size(GREMag));
+weight = zeros(size(GREMag));
+  for nEchos = 1:length(TE)
+    Magtemp(:,:,:,nEchos) = TE(nEchos).*GREMag(:,:,:,nEchos);
+  end
+  Magtotal = sum(Magtemp,4);
+  e = 0.00001;
+  for nEchos = 1:length(TE)
+  weight(:,:,:,nEchos) = Magtemp(:,:,:,nEchos)./ (Magtotal+e);  
+  end
+end
